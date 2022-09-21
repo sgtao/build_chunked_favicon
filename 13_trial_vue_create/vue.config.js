55a256c62d1b379535ecf58838495fd9fe9ec171
Shogo Ogami <sg.tao.so@gmail.com>
2022-09-22 08:39:02 +0900

    update 13_trial for trial use copyPlugin, but not reach goal

--- 13_trial_vue_create/vue.config.js
+++ 13_trial_vue_create/vue.config.js
@@ -1,6 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs/',
+  configureWebpack: {
+    plugins: [
+      /* config.plugin('copy') */
+      new CopyPlugin(
+        {
+          patterns: [
+            {
+              from: 'public/favicon.ico',
+              to: 'favicon[hash].ico',
+              toType: 'file',
+              noErrorOnMissing: true,
+              globOptions: {
+                ignore: [
+                  '**/.DS_Store',
+                ]
+              },
+              info: {
+                minimized: true
+              }
+            }
+          ]
+        }
+      ),
+    ]
+  },
 })
