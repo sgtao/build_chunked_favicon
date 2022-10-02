8f12da0dc64fdd3b9d5914c490e075368222b8b9
Shogo Ogami <sg.tao.so@gmail.com>
2022-10-02 15:04:32 +0900

    update 13_trial_vue for remove unused setting of vue.config.js

--- 13_trial_vue_create/vue.config.js
+++ 13_trial_vue_create/vue.config.js
@@ -1,37 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 // 
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs/',
-  configureWebpack: {
-    // chainWebpack: config => {
-    //   // dist のindex.htmlが重複して吐き出されないようしている。
-    //   config
-    //     .plugin('copy')
-    //     .uses.clear()
-    // },
-    plugins: [
-      /* config.plugin('copy') */
-      // new CopyPlugin({
-      //   patterns: [
-      //     {
-      //       from: 'public/favicon.ico',
-      //       to: '[name].[contenthash][ext]',
-      //     },
-      //   ],
-      // }),
-      /* config.plugin('html') */
-      // new HtmlWebpackPlugin({
-      //   template: "./public/index.html",
-      //   inject: "body",
-      //   favicon: 'public/images/favicon.ico', // faviconはwebpackで指定する(ソースは削除)※
-      //   hash: true, // This is useful for cache busting
-      // }),
-    ],
-  },
   chainWebpack: (config) => {
     //* HTMLの設定 *//
     config
